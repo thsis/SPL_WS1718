61507e14a53b96e5f8e8af77d03e2457c39676d3
thsis <thomas.siskos91@gmail.com>
2018-01-14 16:58:31 +0100

    Add bivariate and multivariate Optimization routine.

--- LDA/utils.R
+++ LDA/utils.R
@@ -1,7 +1,10 @@
-objective = function(x){
-  return(0.008*x^4 + 2)
-}
+# Define Optimization Routines:
+#     - univariate Case.
+#     - bivariate Case.
+#     - multivariate Case.
 
+# ==============================================================================
+# Univariate Case:
 univariateMinimizer = function(obj, learn_rate = 1, epsilon = 1e-3,
                                max_iter = 100, precision = 1e-6, verbose = FALSE,
                                report_freq = 100){
@@ -15,7 +18,6 @@ univariateMinimizer = function(obj, learn_rate = 1, epsilon = 1e-3,
     gradient = min(abs(gradient), 10) * sign(gradient) # Correct the sign
     return(gradient)
   }
-  curve(obj(x), from = -10, to = 10, main = "Objective Function", asp = 1)
   
   # Draw multiple random starting points.
   # Take the combination that provides the lowest value of the objective function.
@@ -40,14 +42,134 @@ univariateMinimizer = function(obj, learn_rate = 1, epsilon = 1e-3,
           sep = c("\t", "\n"))
       points(a, obj(a), col = "red")}
   }
-  cat("\nFinal", "Results:\n","Iteration:", i, "x:", a, "f(x):", obj(a), "df(x):", d(a),
+  cat("\t", "Results:\n","Iteration:", i, "x        :", a, "f(x)     :", obj(a), "df(x)    :", d(a),
       sep = c("\t", "\n"))
   
-  points(a, obj(a), col = "blue", pch = 4, cex = 2)
   if(i >= max_iter){
-    warning("Maximum number of iterations reached.")
-  }
+    warning("Maximum number of iterations reached.")}
+  return(a)
 }
 
-univariateMinimizer(obj = objective, max_iter = 1000000, precision = 1e-9, learn_rate = 0.75, verbose = F)
+# ==============================================================================
+# Bivariate Case:
+bivariateMinimizer = function(obj, epsilon_step = 0.001, max_iter = 10,
+                              precision = 1e-6, learn = 0.5, verbose = FALSE,
+                              report_freq = 100){
+  # Approximate Gradient
+  get_2D_gradient = function(x, objective = obj, epsilon = epsilon_step){
+    init = matrix(data = x, nrow = 2, ncol=2, byrow = TRUE)
+    steps = init + diag(x = epsilon, ncol = 2, nrow = 2)
+    
+    f_steps = apply(steps, 2, objective)
+    f_comp =  apply(init, 2, objective)
+    
+    D = (f_steps - f_comp) / epsilon
+    # Trim D to limit the gradient:
+    D_trimmed = ifelse(abs(D) <= 5, abs(D), 5) * sign(D)
+    return(D_trimmed)
+  }
+  # Draw multiple random starting points.
+  # Take the combination that provides the lowest value of the objective function.
+  a = matrix(data = runif(10000, min = -100, max = 100),
+             ncol = 2)
+  # DEBUG:
+  f_a = apply(a, 1, obj)
+  a = a[which.min(f_a), ]
+  gradient = get_2D_gradient(a)
+  
+  i = 0
+  
+  learn_rates = seq(from = learn, to = 0, length.out = max_iter + 1)
+  
+  while(i <= max_iter & any(abs(gradient) >= precision)){
+    if(i %% report_freq == 0 & verbose) {
+      cat("\nStep:\t\t", i,
+          "\nx:\t\t", a,
+          "\ngradient:\t", gradient,
+          "\nlearn:\t", learn_rates[i], 
+          "\n---------")}
+    
+    i = i + 1
+    a = a - learn_rates[i] * gradient
+    gradient = get_2D_gradient(a)
+  }
+  
+  cat("\nResults\n",
+      "\nIteration:\t", i,
+      "\nx:\t\t", a,
+      "\nf(x):\t\t", obj(a),
+      "\ndf(x):\t\t", gradient)
+  
+  if(i >= max_iter){
+    warning("Maximum number of iterations reached.")}
+  return(a)
+}
 
+# ==============================================================================
+# Multivariate Case:
+gradientDescentMinimizer = function(obj, n_pars, epsilon_step = 0.001,
+                                    max_iter = 10, precision = 1e-6, 
+                                    learn = 0.5, verbose = FALSE, 
+                                    report_freq = 100){
+  # Check for invalid parameters:
+  stopifnot(epsilon_step > 0,
+            max_iter > 0,
+            precision > 0,
+            learn > 0,
+            report_freq > 0)
+  
+  # Approximate Gradient
+  get_gradient = function(x,
+                          d = n_pars,
+                          objective = obj,
+                          epsilon = epsilon_step){
+    init = matrix(data = x, nrow = d, ncol = d, byrow = TRUE)
+    steps = init + diag(x = epsilon, ncol = d, nrow = d)
+    
+    f_steps = apply(steps, 1, objective)
+    f_comp =  apply(init, 1, objective)
+    
+    D = (f_steps - f_comp) / epsilon
+    # Trim D to limit the gradient:
+    D_trimmed = ifelse(abs(D) <= 100, abs(D), 100) * sign(D)
+    return(D_trimmed)
+  }
+  
+  # Draw multiple random starting points.
+  # Use the one that provides the lowest value of the objective function.
+  a = matrix(data = runif(1000 * n_pars, 
+                          min = -100,
+                          max = 100),
+             ncol = n_pars)
+  f_a = apply(a, 1, obj)
+  a = a[which.min(f_a), ]
+  gradient = get_gradient(a)
+  
+  # Set up values for loop
+  i = 0
+  learn_rates = seq(from = learn, to = 0, length.out = max_iter + 1)
+  
+  while(i <= max_iter & any(abs(gradient) >= precision)){
+    if(i %% report_freq == 0 & verbose) {
+      cat("\nStep:\t\t", i,
+          "\nx:\t\t", a,
+          "\ngradient:\t", gradient,
+          "\nlearn:\t", learn_rates[i], 
+          "\n------------------------------------")}
+    
+    i = i + 1
+    a = a - learn_rates[i] * gradient
+    gradient = get_gradient(a)
+  }
+  
+  cat("\nResults\n",
+      "\nIteration:\t", i,
+      "\nx:\t\t", a,
+      "\nf(x):\t\t", obj(a),
+      "\ndf(x):\t\t", gradient,
+      "\n")
+  
+  if(i >= max_iter){
+    warning("Maximum number of iterations reached.")}
+  return(a)
+}
