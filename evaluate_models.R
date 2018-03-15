f3d9774610dfa82ea2e20111ea98413885735027
thsis <thomas.siskos91@gmail.com>
2018-03-15 20:33:56 +0100

    add term-paper checkpoint. Convert evaluate_model()-ROC-plot into type='s'.

---
+++ evaluate_models.R
@@ -0,0 +1,15 @@
+source("Evaluation/Evaluate_Predictions.fin.R")
+
+rf = read.csv("Random_Forest/rf_pred.csv")
+cart = read.csv("CART/cart_pred.csv")
+logit = read.csv("Logit/logit_pred.csv")
+
+summary(rf)
+summary(cart)
+summary(logit)
+
+
+evaluate_model(fitted_probs = rf$X1, labels = rf$label)
+evaluate_model(fitted_probs = cart$X1, labels = cart$label)
+evaluate_model(fitted_probs = logit$X1, labels = logit$labels)
+
